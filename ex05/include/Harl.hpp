4fb932f1425a1f5ebeb4b2479d999006734b55a0
Ezuker <benoit.carolle.pro@gmail.com>
2024-03-15 19:07:05 +0100

    ex05 done + fclean ex04

---
+++ ex05/include/Harl.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bcarolle <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/15 17:32:00 by bcarolle          #+#    #+#             */
+/*   Updated: 2024/03/15 17:35:00 by bcarolle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+# define HARL_HPP
+
+# include <iostream>
+
+class Harl
+{
+    public:
+        //Constructors
+        Harl();
+        ~Harl();
+
+        //Public Member Functions
+        void    complain(std::string level);
+
+    private:
+        //Private Member Functions
+        void    debug(void);
+        void    info(void);
+        void    warning(void);
+        void    error(void);
+};
+#endif
---
+++ ex05/src/main.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bcarolle <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/15 17:32:05 by bcarolle          #+#    #+#             */
+/*   Updated: 2024/03/15 19:05:36 by bcarolle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+int main()
+{
+    Harl    Harl;
+
+    Harl.complain("debug");
+    Harl.complain("info");
+    Harl.complain("warning");
+    Harl.complain("error");
+    Harl.complain("test");
+
+    return (0);
+}
